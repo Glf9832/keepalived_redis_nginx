7c18dc344e64d375fa5df6c051580878242c3c2a
Greene <greene9832@gmail.com>
2019-03-03 22:12:10 +0800

    update keepalived script for check service

--- master/keepalived/scripts/check_service_health.sh
+++ master/keepalived/scripts/check_service_health.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-###/etc/keepalived/scripts/redis_check.sh
 
 ALIVE=`/usr/local/redis/bin/redis-cli PING`
 LOGFILE="/var/log/keepalived/keepalived-redis-check.log"
@@ -8,13 +7,18 @@ pid=$$
 HOSTADDRESS=$(ifconfig eth0 | grep -w "inet" | awk '{print $2}')
 curl_nginx=$(curl -LI http://$HOSTADDRESS -o /dev/null -w '%{http_code}\n' -s)
 
+log() {
+    local msg=$1
+    echo -e "$(date +'%Y-%m-%d %H:%M:%S') | $pid | $msg" >> $LOGFILE 2>&1
+}
+
 for ((i=0; i<2; i++))
 do
     if [ "$ALIVE" == "PONG" ] && [ $curl_nginx == 200 ]; then
-        echo "`date +'%Y-%m-%d %H:%M:%S'`|$pid|state:[check] Success: PING $ALIVE " >> $LOGFILE 2>&1
         exit 0
     else
-        echo "`date +'%Y-%m-%d %H:%M:%S'`|$pid|state:[check] Failed: PING $ALIVE " >> $LOGFILE 2>&1
+        log "[REDIS] Redis PING state: $ALIVE"
+        log "[NGINX] Nginx curl code: $curl_nginx"
         sleep 1
     fi
 done
