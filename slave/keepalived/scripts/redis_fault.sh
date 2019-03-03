b59ac96b2397ddb525e3fd1b6b40976427e87000
Greene <greene9832@gmail.com>
2019-03-03 22:01:02 +0800

    update scripts

--- slave/keepalived/scripts/redis_fault.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-###/etc/keepalived/scripts/redis_fault.sh
-
-LOGFILE="/var/log/keepalived/keepalived-redis-state.log"
-pid=$$
-
-echo "`date +'%Y-%m-%d %H:%M:%S'`|$pid|state:[fault]" >> $LOGFILE 2>&1
-
